688ed26efc238e77e00d0dbe208a74ef787ded93
cdk <everydance075@gmail.com>
2026-03-28 13:30:24 +0800

    Update connection.lua

--- connection.lua
+++ connection.lua
@@ -26,52 +26,60 @@ cnctables.__index = cnctables
 
 function cnctables.new()
 	local self = setmetatable({}, cnctables)
-	
+
 	self.connections = {}
 	self.refreshdata = {}
-	
+
 	return self
 end	
 
 
 --creates connection!
 function cnctables:createconnection(sl, cb, isrefresh: boolean)
-	
-	
+
+
 	local connection = sl:Connect(cb)
-	
+	  
+	  
 	table.insert(self.connections, connection)
-	
+
 	-- if refresh is used it will create a new connection and store it 	 the refreshdata
 	if isrefresh then
-		table.insert(self.refreshdata, {signal = sl, callback = cb})
+		local exists = false
+		for _, data in ipairs(self.refreshdata) do
+			if data.signal == sl and data.callback == cb then
+				exists = true
+				break
+			end
+		end
+		if not exists then
+			table.insert(self.refreshdata, {signal = sl, callback = cb})
+		end
 	end
 
 
-
 	warn(self.connections) 
-	
+
 	return connection
 end
 
 function cnctables:getconnections()
-    return self.connections
+	return self.connections
 end
 
 function cnctables:clearconnections()
-	
+
 	for _, v in ipairs(self.connections) do
 		v:Disconnect()
-		
+
 	end
-	
+
 	-- creates a simple connection
 	for _, data in ipairs(self.refreshdata) do
-		self:createconnection(data.signal, data.callback, false) --"false" to avoid duplications
+		self:createconnection(data.signal, data.callback, false)
 	end
-	
+
 	table.clear(self.connections)
 end
-	
-return cnctables
 
+return cnctables
