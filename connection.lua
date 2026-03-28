430d40e8f672bcdb80c8c8de380251ce3fc371cb
cdk <everydance075@gmail.com>
2026-03-28 11:46:16 +0800

    simpleconnnection
    idk

---
+++ connection.lua
@@ -0,0 +1,77 @@
+--[[ 
+
+   @cdk 2026
+   
+   @Simple Connection
+   
+   how to use this?
+   
+   module.new() <-- init your connection
+   
+   module.new():createconnection(signal, callback, isrefresh)
+   
+   "isrefresh" is used then it creates another like "clone" of the connection then deletes the old one...
+   
+   
+   module.new():clearconnections() <-- clears all connections
+   
+   module.new():getconnections() <-- gets all connections
+   
+]]-- 
+
+local cnctables = {}
+cnctables.__index = cnctables
+
+
+function cnctables.new()
+	local self = setmetatable({}, cnctables)
+	
+	self.connections = {}
+	self.refreshdata = {}
+	
+	return self
+end	
+
+
+--creates connection!
+function cnctables:createconnection(sl, cb, isrefresh: boolean)
+	
+	
+	local connection = sl:Connect(cb)
+	
+	table.insert(self.connections, connection)
+	
+	
+	-- if refresh is used it will create a new connection and store it in the refreshdata
+	if isrefresh then
+		self.refreshdata.signal = connection
+		self.refreshdata.callback = cb
+	end
+	
+	
+	warn(self.connections) 
+	
+	return connection
+end
+
+function cnctables:getconnections()
+    return self.connections
+end
+
+function cnctables:clearconnections(isrefresh : boolean)
+	
+	for _, v in ipairs(self.connections) do
+		v:Disconnect()
+		
+	end
+	
+	-- creates a simple connection
+	if isrefresh and self.refreshdata.signal and self.refreshdata.callback then
+		self:createconnection(self.refreshdata.signal, self.refreshdata.callback, true)
+	end
+	
+	table.clear(self.connections)
+end
+	
+return cnctables
+
