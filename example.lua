1ddff16bab09ea9b92c99d29b9b4f8b6e9fff35d
cdk <everydance075@gmail.com>
2026-03-28 13:28:24 +0800

    Update module name and clear connections
    Renamed module from 'connection' to 'cntion' and added connection clearing.

--- example.lua
+++ example.lua
@@ -1,17 +1,19 @@
 local part = script.Parent
 local clickdetector = part:WaitForChild("ClickDetector")
 
-local connectionModule = require(script.connection)
+local connectionModule = require(script.cntion)
 
 local connection = connectionModule.new()
 
 connection:createconnection(clickdetector.MouseClick, function()
 	print('true')
-end, true )
-
-
 
 	
+end, true)
+	
+connection:clearconnections() -- refresh the refreshable connections
+
+
 
 
 
