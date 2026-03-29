a3d11acf6b394ae5f56de9f4e6b77f0754f29977
cdk <everydance075@gmail.com>
2026-03-29 10:10:57 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -6,15 +6,15 @@ local connectionModule = require(script.cntion)
 local connection = connectionModule.new()
 
 --true, when function run, it creates new "clone" itself
-connection:createconnection(clickdetector.MouseClick:Connect(function)
-		print('true)
-end, true)
+cnt:createconnection(color1_part.ClickDetector.MouseClick:Connect(function()
+	print(true)
+end), true)
 
 --false, when function run, it disconnects 
 
-connection:createconnection(clickdetector.MouseClick:Connect(function)
-		print('true)
-end, false)
+cnt:createconnection(color1_part.ClickDetector.MouseClick:Connect(function()
+	print(true)
+end), false)
 
 
 --no need to put clearconnections() because it is handle by the module
